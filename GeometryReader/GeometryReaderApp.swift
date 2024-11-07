2e0ddff24268bb7d0ab1d2a08068159ee153c577
abemaprog <appdev.abe@gmail.com>
2024-11-07 13:53:34 +0900

    Initial Commit

---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Manato Abe on 2024/11/07.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
